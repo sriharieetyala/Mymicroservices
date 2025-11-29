e0ed1014141d0a319eae5995b90ce4dafbada457
sriharieetyala <sriharieetyala143@gmail.com>
2025-11-29 16:11:57 +0530

    Config serrver

--- ConfigSever/src/test/java/com/practice/configserver/CongigSeverApplicationTests.java
+++ ConfigSever/src/test/java/com/practice/configserver/CongigSeverApplicationTests.java
@@ -1,13 +1,13 @@
-package com.mmicro.questionservice;
+package com.practice.configserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class QuestionServiceApplicationTests {
+class CongigSeverApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
